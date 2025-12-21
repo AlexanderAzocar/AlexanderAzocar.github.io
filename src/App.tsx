ffe4e6dc6c5d47193830bc2554e19ecb1876d8e5
Alexander Azocar <Alexander27azocar@gmail.com>
2025-12-20 20:43:59 -0400

    Init/project structure with react and Shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import HomePage from '@/module/portfolio/pages/HomePage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <HomePage></HomePage>
+    </>
+  )
+}
+
+export default App
